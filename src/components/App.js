1191cef2f7af8cec92065b3303b880bac6fc1f99
senyprim-asus <senyprim105@gmail.com>
2021-11-16 22:51:23 +1000

    int

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,90 @@
 import React from "react";
 import Counter from "./Counter"
-export default function () {
-    const min=1,max=5,value=2;
-    return (
-        <div className="app">
-            <Counter
-            min={min}
-            max={max}
-            />
-        </div>
-    )
+
+
+
+export default class extends React.Component {
+    state = {
+        products: [
+            {
+                id: 100,
+                title: 'Ipnone 200',
+                price: 12000,
+                rest: 10,
+                count: 1
+            },
+            {
+                id: 101,
+                title: 'Samsung AAZ8',
+                price: 22000,
+                rest: 5,
+                count: 1
+            },
+            {
+                id: 103,
+                title: 'Nokia 3310',
+                price: 5000,
+                rest: 2,
+                count: 1
+            },
+            {
+                id: 105,
+                title: 'Huawei ZZ',
+                price: 15000,
+                rest: 8,
+                count: 1
+            }
+        ]
+    };
+
+    change = (newCount, id) => {
+        console.log(`change:${newCount} id:${id}`);
+        const products = this.state.products;
+        const index = products.findIndex(item => item.id === id);
+        const newProducts = [...products.slice(0, index),
+        { ...products[index], ...{ count: newCount } },
+        ...products.slice(index + 1)];
+        
+        this.setState({ products:newProducts },()=>{
+            console.log(this.state);
+        });
+    }
+
+    render() {
+        const productsRows = this.state.products.map((item, index) => {
+            const { id, title, price, rest, count } = item;
+            const total = price * count;
+            return (
+                <tr key={`${id}:${0}:${rest}:${count}`}>
+                    <td>{title}</td>
+                    <td>{price}</td>
+                    <td>
+                        <Counter
+                            min={0}
+                            max={rest}
+                            onChange={(newCount) => this.change(newCount, id)}
+                            count={count}
+                        />
+                    </td>
+                    <td>{total}</td>
+                </tr>
+            )
+        });
+        return (
+            <div>
+                <h2>Cart</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Title</td>
+                            <td>Price</td>
+                            <td>Count</td>
+                            <td>Total</td>
+                        </tr>
+                        {productsRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,31 +5,59 @@ export default class extends React.Component {
   static propTypes = {
     min: PropTypes.number.isRequired,
     max: PropTypes.number.isRequired,
-    value: PropTypes.number,
+    count: PropTypes.number,
+    onChange:PropTypes.func.isRequired
   };
+  constructor(props){
+    super(props);
+    console.log(`Counter.constructor:${props}`);
+  }
   state = {
-    value: this.props.min,
+    inputValue: this.props.count,
   };
 
   trunkValue = (value) => {
     return Math.max(this.props.min, Math.min(value, this.props.max));
   };
 
-  increase = () => this.set(this.state.value + 1);
-  decrease = () => this.set(this.state.value - 1);
-  set = (value) => {
+  increase = () => this.changeCount(this.props.count + 1);
+  decrease = () => this.changeCount(this.props.count - 1);
+  //меняем count
+  changeCount = (value) => {
     value = this.trunkValue(value);
-    this.setState({ value });
+    console.log(`changeCount:${value}`);
+    this.props.onChange(value);
   };
+  //При смене в input записать изменения в state
+  setInputValue=(inputValue)=>{
+    this.setState({inputValue})
+  }
+  //При потере фокуса меняем count
+  acceptInputValue=()=>{
+    const inputValue = parseInt(this.state.inputValue);
+    this.changeCount(isNaN(inputValue)?this.props.min:inputValue);
+  }
 
   render() {
     return (
       <div className="counter">
-        <button className="decrease" onClick={this.decrease}>
+        <button 
+        className="decrease" 
+        onClick={this.decrease}
+        disabled={this.props.count===this.props.min}
+        >
           -
         </button>
-        <span>{this.state.value}</span>
-        <button className="increase" onClick={this.increase}>
+        <input 
+        value={this.state.inputValue}
+        onChange={(evt)=>this.setInputValue(evt.target.value)}
+        onBlur={this.acceptInputValue}
+        />
+        <button 
+        className="increase" 
+        onClick={this.increase}
+        disabled={this.props.count===this.props.max}
+        >
           +
         </button>
       </div>
