cddc06f70b87fb8d044edc5849afb72bb2bc6d37
seny <seny_ussur@mail.ru>
2021-11-21 20:01:25 +1000

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
-const path = require('path');
-const { mainModule } = require('process');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: "./src/main.js",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+    entry: "./src/index.tsx",
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
     },
+    // devServer: { contentBase: path.join(__dirname, "src") },
     module: {
         rules: [
             {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ["@babel/plugin-transform-react-jsx"]
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader:"babel-loader",
+                    options:{
+                        presets: ['@babel/preset-env',"@babel/preset-react"],
                     }
-                }
-            }
-        ]
+                }],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader:"babel-loader",
+                    options:{
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"
+                        ],
+                    }
+                }],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
     },
-}
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+};
